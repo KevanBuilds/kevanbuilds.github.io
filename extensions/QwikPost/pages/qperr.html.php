ef8904d15945b9b7f850ada580ade8c283763519
KevanBuilds <kevanweerdenburg@gmail.com>
2014-10-15 12:07:08 +0200

    upload nle2.0
    test

--- extensions/QwikPost/pages/qperr.html.php
+++
@@ -1,9 +0,0 @@
-<div id="Form" class="Account Preferences" >
-<fieldset>
-  <legend>QwikPost Configuration</legend>
-  <form method="post" action="">
-  <p class="Description">There was an error loading the config files</p>
-  <p></p>
-  </form>
-</fieldset>
-</div>
